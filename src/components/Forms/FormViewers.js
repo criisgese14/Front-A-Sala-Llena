c0ea9cb70329aca89925856df4d9bc18258d9a33
Alejandro Medina <alejandro.medina.blanco@gmail.com>
2022-02-09 14:12:09 +0100

    Formulario viewers

--- src/components/Forms/FormViewers.js
+++ src/components/Forms/FormViewers.js
@@ -1,177 +1,148 @@
-import React, {useEffect, useState, Fragment} from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useHistory } from "react-router-dom";
+import React, {/* useEffect, */ useState, Fragment} from "react";
+import { useDispatch, /* useSelector */ } from "react-redux";
+// import { Link, useHistory } from "react-router-dom";
 import {postViewer} from "../../redux/actions/index.js"
-import Select from "react-select"
+// import Select from "react-select"
 import { useForm } from 'react-hook-form'
 
 const FormViewers = ()=> {
     let dispatch = useDispatch();
-    let history = useHistory();
-    const {register, errors, handleSubmit} = useForm();
-    useEffect(()=>{
-        
-    }, [dispatch])
+    // let history = useHistory();
+    const { register, handleSubmit,  formState: { errors } } = useForm();
     const [input, setInput]= useState({
-        name: "",
-        email: "",
-        password: "",
-        image: "",
-        province: "",
+        
     })
-
-    const onSubmit = data => console.log(data)
-    // const [confirmacion, setConfirmacion] = useState({
-    //     password:"",
-    //     email: "",
-    // })
-    const optionsProvince=[
-        {name: "province",value:"Buenos Aires" ,label:"Buenos Aires"},
-        {name: "province",value:"Cordoba" ,label:"Cordoba"},
-        {name: "province",value:"Santa Fe" ,label:"Santa Fe"},
-        {name: "province",value:"Catamarca" ,label:"Catamarca"},
-        {name: "province",value:"Buenos Aires" ,label:"Buenos Aires"},
-        {name: "province",value:"Catamarca" ,label:"Catamarca"},
-        {name: "province",value:"Chaco" ,label:"Chaco"},
-        {name: "province",value:"Chubut" ,label:"Chubut"},
-        {name: "province",value:"Entre Rios" ,label:"Entre Rios"},
-        {name: "province",value:"Corrientes" ,label:"Corrientes"},
-        {name: "province",value:"Formosa" ,label:"Formosa"},
-        {name: "province",value:"Jujuy" ,label:"Jujuy"},
-        {name: "province",value:"La Pampa" ,label:"La Pampa"},
-        {name: "province",value:"La Rioja" ,label:"La Rioja"},
-        {name: "province",value:"Mendoza" ,label:"Mendoza"},
-        {name: "province",value:"Misiones" ,label:"Misiones"},
-        {name: "province",value:"Neuquen" ,label:"Neuquen"},
-        {name: "province",value:"Rio Negro" ,label:"Rio Negro"},
-        {name: "province",value:"Salta" ,label:"Salta"},
-        {name: "province",value:"San Juan" ,label:"San Juan"},
-        {name: "province",value:"San Luis" ,label:"San Luis"},
-        {name: "province",value:"Santa Cruz" ,label:"Santa Cruz"},
-        {name: "province",value:"Santiago del Estero" ,label:"Santiago del Estero"},
-        {name: "province",value:"Tierra del Fuego" ,label:"Tierra del Fuego"},
-        {name: "province",value:"Tucuman" ,label:"Tucuman"},
-        {name: "province",value:"CABA" ,label:"CABA"},
-    ]
-    // const handleInputChangeViewere = (e) => {
-    //     e.preventDefault();
-    //     setInput({
-    //         ...input,
-    //         [e.target.name]: e.target.value
-    //         });
-    // };
-
-    // const handleInputChangeConfirmacion = (e) => {
-    //     e.preventDefault();
-    //     setConfirmacion({
-    //         ...confirmacion,
-    //         [e.target.name]: e.target.value
-    //         });
-    // };
-
-    // function handleInputChange(e) {
-    //     setInput({
-    //         ...input,
-    //         [e.name]: e.value,
-    //     });
-    // }
-
-
     
-    // function handleSubmit(e) {
-    //     if(input.password !== confirmacion.password) {
-    //         e.preventDefault();
-    //         alert('Las contraseñas no son iguales')
-    //     }else if(input.email !== confirmacion.email) {
-    //         e.preventDefault();
-    //         alert('Los correos electronicos no coinciden')
-    //     }else{
-    //     e.preventDefault();
-    //     dispatch(postViewer(input));
-    //     alert("Usuario Creado!");
-    //     setInput({
-    //     name: "",
-    //     email: "",
-    //     password: "",
-    //     image: "",
-    //     province: "",});
-    //     history.push("/");}
-        
-        
-    // }
+
+    const onSubmit = (data) => {
+        setInput(data)
+        postViewer(input)
+    }
+    
+    // const optionsProvince=[
+    //     {name: "province",value:"Buenos Aires" ,label:"Buenos Aires"},
+    //     {name: "province",value:"Cordoba" ,label:"Cordoba"},
+    //     {name: "province",value:"Santa Fe" ,label:"Santa Fe"},
+    //     {name: "province",value:"Catamarca" ,label:"Catamarca"},
+    //     {name: "province",value:"Buenos Aires" ,label:"Buenos Aires"},
+    //     {name: "province",value:"Catamarca" ,label:"Catamarca"},
+    //     {name: "province",value:"Chaco" ,label:"Chaco"},
+    //     {name: "province",value:"Chubut" ,label:"Chubut"},
+    //     {name: "province",value:"Entre Rios" ,label:"Entre Rios"},
+    //     {name: "province",value:"Corrientes" ,label:"Corrientes"},
+    //     {name: "province",value:"Formosa" ,label:"Formosa"},
+    //     {name: "province",value:"Jujuy" ,label:"Jujuy"},
+    //     {name: "province",value:"La Pampa" ,label:"La Pampa"},
+    //     {name: "province",value:"La Rioja" ,label:"La Rioja"},
+    //     {name: "province",value:"Mendoza" ,label:"Mendoza"},
+    //     {name: "province",value:"Misiones" ,label:"Misiones"},
+    //     {name: "province",value:"Neuquen" ,label:"Neuquen"},
+    //     {name: "province",value:"Rio Negro" ,label:"Rio Negro"},
+    //     {name: "province",value:"Salta" ,label:"Salta"},
+    //     {name: "province",value:"San Juan" ,label:"San Juan"},
+    //     {name: "province",value:"San Luis" ,label:"San Luis"},
+    //     {name: "province",value:"Santa Cruz" ,label:"Santa Cruz"},
+    //     {name: "province",value:"Santiago del Estero" ,label:"Santiago del Estero"},
+    //     {name: "province",value:"Tierra del Fuego" ,label:"Tierra del Fuego"},
+    //     {name: "province",value:"Tucuman" ,label:"Tucuman"},
+    //     {name: "province",value:"CABA" ,label:"CABA"},
+    // ]
     console.log(input);
     // console.log(confirmacion)
     return(
-        <div>
             <Fragment>
                 <form  onSubmit={handleSubmit(onSubmit)}>
                     <label>Nombre:</label>
                     <input  type="text" 
                             name="name" 
-                            /* value={input.name}  */
-                            placeholder="Ingresa tu Nombre" 
-                            /* onChange={(e)=>{handleInputChangeViewere(e)}} */
-                            ref={
-                                register({
-                                    required: {value:true, message: 'Ingrese un nombre'}
-                                })
-                            }/>
-                            <span className="text-danger text-small d-block mb-2">
-                            {errors.usuario && errors.usuario.message}
-                            </span>
+                            placeholder="Ingrese su Nombre"
+                            className="form-control my-2"
+                            {...register("name" ,{
+                                required:{ 
+                                value: true,
+                                message: "El campo es requerido",
+                                }
+                            })}
+                            />
+                            {<span className="text-danger text-small d-block mb-2">{errors.name && errors.name.message}</span>}
 
-                    <label>Email:</label>
-                    <input  type="email" 
-                            title= "Introduzca una direccion de correo valida ejem: campos@gmail.com" 
-                            name="email" /* value={input.email} */ placeholder="@gmail.com" 
-                            /* onChange={(e)=>{handleInputChangeViewere(e)}} *//>
+                            <label>Email:</label>
+                            <input  type="text" 
+                            name="email" 
+                            placeholder="Ingrese su Correo Electronico"
+                            className="form-control my-2"
+                            {...register("email" ,{
+                                required:{ 
+                                value: true,
+                                message: "El campo es requerido",
+                                },
+                                pattern: {
+                                    value: /\S+@\S+\.\S+/,
+                                    message: "Este formato de correo no es el adecuado"
+                                }
+                            })}
+                            />
+                            {<span className="text-danger text-small d-block mb-2">{errors.email && errors.email.message}</span>}
 
-                    {/* <label>Repite el Email:</label>
-                    <input  type="email" 
-                            title= "Introduzca una direccion de correo valida ejem: campos@gmail.com" 
-                            name="email" value={confirmacion.email} placeholder="@gmail.com" 
-                            onChange={(e)=>{handleInputChangeConfirmacion(e)}}/> */}
-
-                    <label>Contraseña:</label>
-                    <input  type="password" 
-                            pattern=".{6,}" name="password" 
-                            title= "Min: 6 caracteres. Solo numeros y letras" 
-                            /* value={input.password} */ placeholder="Contraseña" 
-                            /* onChange={(e)=>{handleInputChangeViewere(e)} *//>
-
-                    <label>Repite la Contraseña:</label>
-                    <input  type="password" 
-                            pattern=".{6,}" name="password" 
-                            title= "Min: 6 caracteres. Solo numeros y letras" 
-                            /* value={confirmacion.password} */ placeholder="Contraseña" 
-                            /* onChange={(e)=>{handleInputChangeConfirmacion(e)}} *//>
+                            <label>Contraseña:</label>
+                            <input  type="password" 
+                            name="password" 
+                            placeholder="Ingrese su Contraseña"
+                            className="form-control my-2"
+                            {...register("password" ,{
+                                required:{ 
+                                value: true,
+                                message: "El campo es requerido",
+                                },
+                                minLength: {
+                                    value: 5,
+                                    message: "Minimo 5 caracteres"
+                                }
+                            })}
+                            />
+                            {<span className="text-danger text-small d-block mb-2">{errors.password && errors.password.message}</span>}
 
                     <label>Imagen de perfil:</label>
                     <input  type="url" 
-                            width="100" height="30" 
-                            alt="Login" name="image"
-                            /* src={input.image} */   checked="checked"
+                            width="100" height="30"  name="image"
                             alt="perfil"
-                            /* value={input.image} */ 
                             placeholder="Inserte una URL de imagen" 
-                            /* onChange={(e)=>{handleInputChangeViewere(e)}} *//>
-
+                            className="form-control my-2"
+                            {...register("image" ,{})}
+                    />
                     <label>Selecciona la Provincia:</label>
-                    <Select options={optionsProvince} 
-                            /* onChange={(e)=>{handleInputChange(e)}}   *//>
-                            
-                            {/* {   input.name===""|| 
-                                input.province===""|| 
-                                input.email===""|| 
-                                input.password===""
-                                ?
-                                <button disabled>Enviar Registro</button>
-                                :
-                                <button type="submit">Enviar Registro</button>} */}
-                    <button type="submit">Enviar Registro</button>
+                    {/* <Select options={optionsProvince} 
+                            onChange={(e)=>{handleInputChange(e)}}/>   */}
+                    <select className="form-control" name="province" {...register("province" ,{})} >
+                        <option default>Seleccione una Provincia</option>
+                        <option>Buenos Aires</option>
+                        <option>Cordoba</option>
+                        <option>Santa Fe</option>
+                        <option>Catamarca</option>                        
+                        <option>Chaco</option>                        
+                        <option>Chubut</option>                        
+                        <option>Entre Rios</option>                        
+                        <option>Corrientes</option>                       
+                        <option>Formosa</option>
+                        <option>Jujuy</option>                        
+                        <option>La Pampa</option>                        
+                        <option>La Rioja</option>                        
+                        <option>Mendoza</option>                        
+                        <option>Misiones</option>
+                        <option>Neuquen</option>                        
+                        <option>Rio Negro</option>                        
+                        <option>Salta</option>                        
+                        <option>San Juan</option>                        
+                        <option>San Luis</option>
+                        <option>Santa Cruz</option>                        
+                        <option>Santiago del Estero</option>                        
+                        <option>Tierra del Fuego</option>                        
+                        <option>Tucuman</option>                        
+                        <option>CABA</option>
+                    </select>
+                    <button className="btn btn-primary">Enviar</button>
                 </form>
             </Fragment>
-        </div>
     )
 }
 export default FormViewers;
