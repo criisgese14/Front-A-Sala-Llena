00213b1d1d97d518f8ecb490e71c84b86452c65c
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-02-09 16:22:19 -0300

    Create Newsletter.js

---
+++ src/components/Newsletter/Newsletter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBarPerfilViewer from "../NavBar/NavBarPerfilViewer";
+
+const Newsletter = () => {
+    return(
+        <div>
+            <NavBarPerfilViewer/>
+        </div>
+    )
+}
+
+export default Newsletter;
