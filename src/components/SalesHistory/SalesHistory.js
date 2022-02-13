70bc5bd59f69664b9edb26fde67ee6aa1aaf1209
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-02-13 19:14:25 -0300

    Update SalesHistory.js

--- src/components/SalesHistory/SalesHistory.js
+++ src/components/SalesHistory/SalesHistory.js
@@ -21,7 +21,7 @@ const SalesHistory = () =>{
     },[]);
     
     
-    console.log(tickets)
+    console.log('prueba',tickets.viewerId)
     let filterShows = show?.filter((e) => e.theaterId ===  theater?.id)
     let filterTicket = tickets?.filter((e) => e.show.theaterId === theater?.id)
     console.log('filter',filterShows)
