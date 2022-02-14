5e32eba320496d7ce6823157fc5d883d8fdd8d8a
Norberto Mangiacone <mangiaconenorberto@gmail.com>
2022-02-14 11:49:01 -0300

    deploy

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -60,7 +60,7 @@ const useUser = () => {
     setKey(null)
     setId(null)
     //setRol(null)
-    window.location.href="https://front-a-sala-llena-black.vercel.app"
+    window.location.href="https://quizzical-colden-ae9e61.netlify.app"
   }, [setKey,setId])
 
   return {
