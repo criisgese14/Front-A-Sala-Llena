1808ef856777706e5e35ac4efcff82371308e330
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-02-14 15:45:45 -0300

    Update UserContext.js

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -9,28 +9,14 @@ export function UserContextProvider ({children}) {
   const [key, setKey] = useState(
     () => window.sessionStorage.getItem('key')
   )
-  const [id,setId] = useState(
-    () => window.sessionStorage.getItem('id')
-  )
-  const [rol,setRol] = useState(
-    () => window.sessionStorage.getItem('rol')
-  )
-
-  const[roles,setRoles] = useState(
-    () => window.sessionStorage.getItem('roles')
-  )
+  
 
   
 
   return <Context.Provider value={{
-    rol,
-    key,
-    setRol,
+    
     setKey,
-    id,
-    setId,
-    roles,
-    setRoles
+    
   }}>
     {children}
   </Context.Provider>
