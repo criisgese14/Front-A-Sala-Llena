088d7fc9ecd60188b23bca5322addbe463fa206f
Lauchis <lauchis892@gmail.com>
2022-02-14 16:04:58 -0300

    saque el filtro de score

--- src/components/LogInTheatres/LogInTheatres.js
+++ src/components/LogInTheatres/LogInTheatres.js
@@ -48,7 +48,7 @@ const LogInTheatres = () => {
     e.preventDefault();
     login(input);
     //navigate('/viewerHome/1')
-    window.location.href = `https://quizzical-colden-ae9e61.netlify.app/theaterHome/${filterTheater.id}/`;
+    window.location.href = `http://localhost:3000/theaterHome/${filterTheater.id}/`;
     setInput({ email: "", password: "" });
   }
 
--- src/components/LogInViewer/LogInViewer.js
+++ src/components/LogInViewer/LogInViewer.js
@@ -60,7 +60,7 @@ const LogInViewer = () => {
     e.preventDefault();
     loginviewer(input);
     //navigate('/viewerHome/1')
-    window.location.href = `https://quizzical-colden-ae9e61.netlify.app/viewerHome/${filterViewer.id}/`;
+    window.location.href = `http://localhost:3000/viewerHome/${filterViewer.id}/`;
     setInput({ email: "", password: "" });
   }
 
