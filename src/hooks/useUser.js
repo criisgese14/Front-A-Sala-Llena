760fe08a60ef212cb542c4fbadf292440a73afc1
Norberto Mangiacone <mangiaconenorberto@gmail.com>
2022-02-15 11:33:36 -0300

    deploy

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,65 +1,70 @@
-import { useCallback, useContext, useState } from "react";
-import Context from "../context/UserContext.js";
-import { loginTheater, loginViewer } from "../redux/actions/index.js";
-import { useSelector } from "react-redux";
+import {useCallback, useContext, useState} from 'react'
+import Context from '../context/UserContext.js';
+import {loginTheater, loginViewer} from '../redux/actions/index.js';
+
 
 const useUser = () => {
-  const { rol, key, id, setRol, setKey, setId, roles, setRoles } =
-    useContext(Context);
-  const [state, setState] = useState({ loading: false, error: false });
+  const {rol, key, id, setRol, setKey, setId, roles, setRoles} = useContext(Context)
+  const [state, setState] = useState({ loading: false, error: false })
   
 
-  const login = useCallback(
-    (input) => {
-      setState({ loading: true, error: false });
-      loginTheater(input)
-        .then((data) => {
-          window.sessionStorage.setItem("key", data);
-          
-          setState({ loading: false, error: false });
-          setKey(data);
-          
-        })
-        .catch((err) => {
-          window.sessionStorage.removeItem("key");
-          
-          setState({ loading: false, error: true });
-          console.error(err);
-        });
-    },
-    [setKey, setId, setRoles]
-  );
+  const login = useCallback((input) => {
+    setState({loading: true, error: false })
+    loginTheater(input)
+        .then(data => {
+          window.sessionStorage.setItem('key', data.token)
+          window.sessionStorage.setItem('id', data.id)
+          window.sessionStorage.setItem('roles', data.isTheater)
+        setState({loading: false, error: false })
+        setKey(data.token)
+        setId(data.id)
+        setRoles(data.isTheater)
+      })
+      .catch(err => {
+        window.sessionStorage.removeItem('key')
+        window.sessionStorage.removeItem('id')
+        window.sessionStorage.removeItem('roles')
+        setState({loading: false, error: true })
+        console.error(err)
+      })
+  }, [setKey,setId,setRoles])
 
-  const loginviewer = useCallback(
-    (input) => {
-      setState({ loading: true, error: false });
-      loginViewer(input)
-        .then((data) => {
-          window.sessionStorage.setItem("key", data);
-          
-          setState({ loading: false, error: false });
-          setKey(data);
-          
-        })
-        .catch((err) => {
-          window.sessionStorage.removeItem("key");
-          
-          setState({ loading: false, error: true });
-          console.error(err);
-        });
-    },
-    [setKey, setId, setRol]
-  );
+  const loginviewer = useCallback((input) => {
+    setState({loading: true, error: false })
+    loginViewer(input)
+        .then(data => {
+        window.sessionStorage.setItem('key', data.token)
+        window.sessionStorage.setItem('id', data.id)
+        window.sessionStorage.setItem('rol', data.isViewer)
+        setState({loading: false, error: false })
+        setKey(data.token)
+        setId(data.id)
+        setRol(data.isViewer)
+      })
+      .catch(err => {
+        window.sessionStorage.removeItem('key')
+        window.sessionStorage.removeItem('id')
+        window.sessionStorage.removeItem('rol')
+        setState({loading: false, error: true })
+        console.error(err)
+      })
+  }, [setKey,setId,setRol])
+
+  
 
   const logout = useCallback(() => {
-    window.sessionStorage.removeItem("key");
-    setKey(null);
-    
+    window.sessionStorage.removeItem('key')
+    window.sessionStorage.removeItem('id')
+    window.sessionStorage.removeItem('rol')
+    window.sessionStorage.removeItem('roles')
+    setKey(null)
+    setId(null)
     //setRol(null)
     window.location.href="https://nmangiacone.github.io/Front-A-Sala-Llena"
-  }, [setKey])
+  }, [setKey,setId])
 
   return {
+    
     isLogged: Boolean(key),
     isLoginLoading: state.loading,
     hasLoginError: state.error,
@@ -68,8 +73,8 @@ const useUser = () => {
     loginviewer,
     id,
     rol,
-    roles,
-  };
-};
+    roles
+  }
+} 
 
 export default useUser;
