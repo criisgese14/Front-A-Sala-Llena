22246b0f4d26f6d32b8b41c92664861e2baccc16
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-02-16 13:13:28 -0300

    Update UserContext.js

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -6,31 +6,16 @@ const Context = React.createContext({})
 
 export function UserContextProvider ({children}) {
   
-  const [key, setKey] = useState(
-    () => window.sessionStorage.getItem('key')
+  const [status, setStatus] = useState(
+    () => window.sessionStorage.getItem('status')
   )
-  const [id,setId] = useState(
-    () => window.sessionStorage.getItem('id')
-  )
-  const [rol,setRol] = useState(
-    () => window.sessionStorage.getItem('rol')
-  )
-
-  const[roles,setRoles] = useState(
-    () => window.sessionStorage.getItem('roles')
-  )
-
+  
   
 
   return <Context.Provider value={{
-    rol,
-    key,
-    setRol,
-    setKey,
-    id,
-    setId,
-    roles,
-    setRoles
+    status,
+    setStatus,
+    
   }}>
     {children}
   </Context.Provider>
